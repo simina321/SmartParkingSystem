2daed6a3ff657430d542b98b8cb08b3312b8f16e
unknown <alexandru.rosca23@yahoo.com>
2018-12-11 21:00:41 +0200

    Add worker

---
+++ Worker/WebApplication1/WebApplication1/Models/ToDoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication1.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base (options)
+        {
+
+        }
+        public DbSet<ParkDetails> ParkDet { get; set; }
+    }
+}
